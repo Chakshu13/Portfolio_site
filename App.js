43c3dbf9c1fd047273227d40413b53e30cd06f6d
Chakshu13 <168061084+Chakshu13@users.noreply.github.com>
2024-12-16 15:33:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+<h1>HOME</h1><h1>ABOUT US</h1>
+
+    </>
+  
+  
+  );
+}
+
+export default App;
---
+++ Nav.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav(){
+    return <header class="header">
+    <div class="logo"><img src="./icons/font-size" alt="" /></div>
+    <div class="purchase-btn"><button class="btn btn-outline-light">PURCHASE NOW</button></div>
+  </header>
+}
+export default Nav;
